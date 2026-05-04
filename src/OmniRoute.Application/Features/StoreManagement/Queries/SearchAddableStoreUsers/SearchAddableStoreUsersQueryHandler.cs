e395c8f8e411f406a5326afc9b5ee6ea005fe210
jayptl11 <phamlong2k3@gmail.com>
2026-05-04 20:15:31 +0700

    update

--- src/OmniRoute.Application/Features/StoreManagement/Queries/SearchAddableStoreUsers/SearchAddableStoreUsersQueryHandler.cs
+++ src/OmniRoute.Application/Features/StoreManagement/Queries/SearchAddableStoreUsers/SearchAddableStoreUsersQueryHandler.cs
@@ -9,8 +9,8 @@ namespace OmniRoute.Application.Features.StoreManagement.Queries.SearchAddableSt
 internal sealed class SearchAddableStoreUsersQueryHandler
     : IQueryHandler<SearchAddableStoreUsersQuery, List<AddableStoreUserDto>>
 {
-    // QL có thể thêm nhân viên SA, CS, DP vào đơn vị
-    private static readonly HashSet<string> AllowedRoles = ["SA", "CS", "DP"];
+    // QL có thể thêm nhân viên SA, CS vào đơn vị (DP là dispatcher trung tâm, không thuộc store)
+    private static readonly HashSet<string> AllowedRoles = ["SA", "CS"];
 
     private readonly IApplicationDbContext _db;
     private readonly ICurrentUserService _currentUserService;
